c8a5463c1c343e32810385d5c99e8642feabaa5f
project-haley <60005425+project-haley@users.noreply.github.com>
2021-09-24 16:44:16 -0500

    Initial commit

---
+++ RoboLoan/Helpers/LoanHelper.cs
@@ -0,0 +1,73 @@
+﻿using RoboLoan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoboLoan.Helpers
+{
+    public class LoanHelper
+    {
+        public Loan GetPayments(Loan loan)
+        {
+            // Calculate monthly payment
+            loan.Payment = CalcPayment(loan.Amount, loan.Rate, loan.Term);
+
+            // Create a loop from 1 to the term
+            var balance = loan.Amount;
+            var totalInterest = 0.0m;
+            var monthlyInterest = 0.0m;
+            var monthlyPrincipal = 0.0m;
+            var monthlyRate = CalcMonthlyRate(loan.Rate);
+
+            for (int month = 0; month <= loan.Term; month++)
+            {
+                monthlyInterest = CalcMonthlyInterest(balance, monthlyRate);
+                totalInterest += monthlyInterest;
+                monthlyPrincipal = loan.Payment - monthlyInterest;
+                balance -= monthlyPrincipal;
+
+                LoanPayment loanPayment = new();
+
+                loanPayment.Month = month;
+                loanPayment.Payment = loan.Payment;
+                loanPayment.MonthlyPrincipal = monthlyPrincipal;
+                loanPayment.MonthlyInterest = monthlyInterest;
+                loanPayment.TotalInterest = totalInterest;
+                loanPayment.Balance = balance;
+
+                loan.PaymentList.Add(loanPayment);
+            }
+
+            loan.TotalInterest = totalInterest;
+            loan.TotalCost = loan.Amount + totalInterest;
+
+
+            return loan;
+        }
+
+        private decimal CalcPayment(decimal amount, decimal rate, int term)
+        {
+            decimal monthlyRate = CalcMonthlyRate(rate);
+
+            var rateD = Convert.ToDouble(monthlyRate);
+            var amountD = Convert.ToDouble(amount);
+
+            var paymentD = (amountD * rateD) / (1 - Math.Pow(1+rateD, -term));
+
+            return Convert.ToDecimal(paymentD);
+        }
+
+        // Convert annual rate to monthly
+        private decimal CalcMonthlyRate(decimal annualRate)
+        {
+
+            return annualRate / 1200;
+        }
+
+        private decimal CalcMonthlyInterest(decimal balance, decimal monthlyRate)
+        {
+            return balance * monthlyRate;
+        }
+    }
+}
